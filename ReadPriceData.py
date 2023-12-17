59c65899e738b72f5bd47a80ec53e4e7183e8013
David Nordin <david@alm-nordin.se>
2023-12-17 21:40:07 +0100

    Update file paths and fix method signature in HeatpumpThrottleSensor.yaml and ReadPriceData.py

--- ReadPriceData.py
+++ ReadPriceData.py
@@ -1,25 +1,26 @@
-# ReadPriceData.py
 import hassapi as hass
 import csv
-from datetime import datetime
+from datetime import datetime, time
 from dateutil.parser import parse
 
-def read_price_data(file_path):
-  with open(file_path, 'r') as file:
-    reader = csv.reader(file)
-    next(reader)  # Skip the header row
+class ReadPriceData(hass.Hass):
 
-    current_hour = datetime.now().hour
-    for row in reader:
-      timestamp = parse(row[0])
-      hour = timestamp.hour
-      if hour == current_hour:
-        data = row[1].split(',')
-        price = data[0].split(':')[1].strip()
-        adjustment = data[2].split(':')[1].strip()
-        print(f'Price: {price}, Adjustment: {adjustment}')
-        break
+  def initialize(self):
+    self.run_daily(self.read_price_data, time(0, 0))
 
-# Usage example
-file_path = '/homeassistant/price_range.csv'
-read_price_data(file_path)
+  def read_price_data(self, kwargs):
+    file_path = '/homeassistant/price_range.csv'
+    with open(file_path, 'r') as file:
+      reader = csv.reader(file)
+      next(reader)  # Skip the header row
+
+      current_hour = datetime.now().hour
+      for row in reader:
+        timestamp = parse(row[0])
+        hour = timestamp.hour
+        if hour == current_hour:
+          data = row[1].split(',')
+          price = data[0].split(':')[1].strip()
+          adjustment = data[2].split(':')[1].strip()
+          print(f'price: {price}, adjustment: {adjustment}')
+          break
